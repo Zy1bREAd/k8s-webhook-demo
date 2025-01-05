4363fe99d009223879fd028edb3440aca1d530d1
Ocean Wang <qq1186405248@126.com>
2025-01-05 20:14:55 +0800

    [Fix] Fix admissionResponse object issue

--- api/hook.go
+++ api/hook.go
@@ -24,12 +24,14 @@ func init() {
 }
 
 func admitPod(ar v1beta1.AdmissionRequest) *v1beta1.AdmissionResponse {
+	admissionResp := &v1beta1.AdmissionResponse{
+		Allowed: true,
+		UID:     ar.UID,
+	}
 	podResource := v1.Resource("pods")
 	if ar.RequestResource.Resource != podResource.Resource {
 		fmt.Println("pod 不相等")
-		return &v1beta1.AdmissionResponse{
-			Allowed: true,
-		}
+		return admissionResp
 	}
 	// 接下来是Pod资源类型，对其先反序列化拿到Pod相关资源数据
 	raw := ar.Object.Raw
@@ -37,28 +39,25 @@ func admitPod(ar v1beta1.AdmissionRequest) *v1beta1.AdmissionResponse {
 	_, _, err := deserializer.Decode(raw, nil, &pod)
 	if err != nil {
 		fmt.Println("反序列化成Go中数据出错", err)
-		return &v1beta1.AdmissionResponse{
-			Allowed: false,
-			Result: &metav1.Status{
-				Message: err.Error(),
-			},
+		admissionResp.Allowed = false
+		admissionResp.Result = &metav1.Status{
+			Message: err.Error(),
 		}
+		return admissionResp
 	}
 
 	// 自定义Hook操作验证
 	for _, c := range pod.Spec.Containers {
+		// 判断pod request是否为0
 		if c.Resources.Requests.Cpu().IsZero() {
-			// 判断pod request是否为0
-			return &v1beta1.AdmissionResponse{
-				Allowed: false,
-				Result: &metav1.Status{
-					Message: "Must have CPU requests.",
-				},
+
+			admissionResp.Allowed = false
+			admissionResp.Result = &metav1.Status{
+				Message: "Must have CPU requests.",
 			}
+			return admissionResp
 		}
 	}
-	return &v1beta1.AdmissionResponse{
-		Allowed: true,
-	}
+	return admissionResp
 
 }
